a5183d95c65a360d2146a202070f0ff9d37479b8
Krzysztof Lamkiewicz <krzysztof.lamkiewicz@gmail.com>
2015-05-20 16:58:13 +0200

    Initial commit with defined test methods

--- StreamsTask/src/tests/CustomerServiceTests.java
+++ StreamsTask/src/tests/CustomerServiceTests.java
@@ -4,23 +4,84 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import services.CustomerService;
 import services.CustomerServiceInterface;
 import entities.Customer;
+import entities.Product;
 
 public class CustomerServiceTests {
 
+	private CustomerService cs;
+	private List<Customer> res;
+	
+	@Before
+	public void testBefore() {
+		cs = new CustomerService(DataProducer.getTestData(10));
+	}
+	
 	@Test
 	public void testFindByName() {
-		CustomerServiceInterface cs = new CustomerService(DataProducer.getTestData(10));
-		
-		List<Customer> res = cs.findByName("Customer: 1");
-		
+		res = cs.findByName("Customer: 1");
 		assertNotNull("Result can't be null", res);
 		assertEquals(1, res.size());
-		
 	}
 
+	@Test
+	public void testFndByField(){
+		//CustomerServiceInterface cs = new CustomerService(DataProducer.getTestData(10));
+		//List<Customer> res = cs.findByField("", value);
+	}
+
+	@Test
+	public void testCustomersWhoBoughtMoreThan(){
+		res = cs.customersWhoBoughtMoreThan(5);
+		
+	}
+	
+	@Test
+	public void testCustomersWhoSpentMoreThan(){
+		res = cs.customersWhoSpentMoreThan(0.2);
+		
+	}
+	
+	@Test
+	public void testCustomersWithNoOrders(){
+		res = cs.customersWithNoOrders();
+		
+	}
+	
+	@Test
+	public void testAddProductToAllCustomers(){
+		
+	}
+	
+	@Test
+	public void testAvgOrders(){
+		double avgOrderWithEmpties = cs.avgOrders(true);
+	}
+	
+	@Test
+	public void testWasProductBought(){
+	
+	}
+	
+	@Test
+	public void testMostPopularProduct(){
+		List<Product> products = cs.mostPopularProduct();
+		
+	}
+	
+	@Test
+	public void testCountBuys(){
+		
+	}
+	
+	@Test
+	public void testCountCustomersWhoBought(){
+		
+	}
+	
 }
